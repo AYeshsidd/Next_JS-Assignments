20440272badfb4dbd0509ab64511a533703f7229
AYeshsidd <159470548+AYeshsidd@users.noreply.github.com>
2024-10-23 02:46:38 -0700

    Delete route-props directory

--- route-props/src/app/[Countries_data]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import Work from "../components/Work"
-
-export default function Country_info({params}:{params:{Countries_data :string}}){
-    const country_props  = params.Countries_data; 
- 
-    return <Work country_props={country_props} />
-}
