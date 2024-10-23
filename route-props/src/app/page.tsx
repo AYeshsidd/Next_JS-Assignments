20bed795d3986b4a382e674e587a36e20610bd4b
AYeshsidd <159470548+AYeshsidd@users.noreply.github.com>
2024-10-23 14:47:58 +0500

    Add files via upload

---
+++ route-props/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+function Home_page(){
+  return(
+    <div>
+
+<ol>
+
+  <li className="text-green-800"><Link href="http://localhost:3000/Pakistan"><b>▪️ Pakistan</b></Link></li>
+   <li className="text-blue-500"><Link href="http://localhost:3000/India"><b>▪️ India</b></Link></li>
+  <li className="text-blue-800"><Link href="http://localhost:3000/Srilanka"><b>▪️ Srilanka</b></Link></li>
+  <li className="text-red-700"><Link href="http://localhost:3000/Afghanistan"><b>▪️ Afghanistan</b></Link></li>
+  <li className="text-green-700"><Link href="http://localhost:3000/Saudia"><b>▪️ Saudia Arabia</b></Link></li>
+  
+</ol>
+    </div>
+  )
+}
+export default Home_page;
